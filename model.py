3dcab106d2e5e5ca2f29cabe172a3d2dea7e5aed
Lana Yang-Maccini <lana_yang-maccini@brown.edu>
2024-05-05 13:14:36 -0400

    r2score working

--- model.py
+++ model.py
@@ -7,15 +7,19 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.callbacks import LearningRateScheduler
 from preprocess import get_data
 
+r2score = tf.keras.metrics.R2Score(class_aggregation="uniform_average", num_regressors=0, name="r2_score")
+
 def lr_schedule(epoch, lr):
     if epoch > 50 and epoch % 20 == 0:
         return lr * 0.9
     return lr
 
-# data = get_data('black-scholes', 1000000)
-data = get_data('heston', 100)
-X_train, Y_train, X_test, Y_test = data
+def r2score_fn(y_true, y_pred):
+    return r2score(y_true, y_pred)
 
+data = get_data('black-scholes', 1000000)
+# data = get_data('heston', 100)
+X_train, Y_train, X_test, Y_test = data
 
 options_pricing_model = Sequential([
     Dense(512, input_dim=X_train.shape[1], kernel_regularizer=l2(0.01)),
@@ -43,17 +47,17 @@ options_pricing_model = Sequential([
     Activation('relu'),
     Dropout(0.3),
 
-    Dense(1, activation='linear', dtype=tf.float32)
+    Dense(1, activation='linear')
 ])
 
 options_pricing_model.compile(optimizer=Adam(learning_rate=0.001),
               loss='mean_squared_error',
-              metrics=['mean_squared_error'
-                    #    ,tf.keras.metrics.R2Score(class_aggregation="uniform_average", num_regressors=0, name="r2_score", dtype=tf.float32)
+              metrics=['mean_squared_error', 
+                       r2score_fn
                        ])
 
 lr_scheduler = LearningRateScheduler(lr_schedule, verbose=1)
 history = options_pricing_model.fit(X_train, Y_train, epochs=200, batch_size=1024, 
                     validation_split=0.1, callbacks=[lr_scheduler])
-test_loss, test_mse = options_pricing_model.evaluate(X_test, Y_test, verbose=2)
+test_loss, test_mse, test_r2score = options_pricing_model.evaluate(X_test, Y_test, verbose=2)
 predictions = options_pricing_model.predict(X_test)
